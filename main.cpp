b263b3e6595ae69d7e6ba363b21ce699702408b9
Faleh Alotaibi <121350271+f-alotaibi@users.noreply.github.com>
2025-07-20 15:40:27 +0300

    Remove old comments

--- main.cpp
+++ main.cpp
@@ -12,22 +12,11 @@ int main() {
     };
     replay.start();
     
-    //std::thread input_hook(hook_loop);
-
     while (true) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-    /*
-    
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    if (!replay.save()) {
-        std::cout << "Could not save replay" << std::endl;
-    };
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    */
-    replay.stop();
 
-    //input_hook.join();
+    replay.stop();
 
     return 0;
 }
