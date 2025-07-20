d140d348f237092b2155b1ef0ca5352a2a90bae0
Faleh Alotaibi <121350271+f-alotaibi@users.noreply.github.com>
2025-07-20 16:41:07 +0300

    (obs) update flags for build scripts

--- external/build_obs.ps1
+++ external/build_obs.ps1
@@ -1,25 +1,32 @@
-$OBSDir = Resolve-Path "obs-studio"
-rmdir -Force -Recurse ./build-windows/
-Set-Location $OBSDir
+rmdir -Force -Recurse ./obs-studio/build-windows/
 
 $Flags = @(
+    # Core
     "-DENABLE_UI=OFF",
     "-DENABLE_SCRIPTING=OFF",
     "-DENABLE_FRONTEND=OFF",
 
+    # Plugins
     "-DENABLE_AJA=OFF",
-    "-DENABLE_SCRIPTING=OFF",
     "-DENABLE_DECKLINK=OFF",
-    "-DENABLE_VIRTUALCAM=OFF",
     "-DENABLE_BROWSER=OFF",
     "-DENABLE_VLC=OFF",
     "-DENABLE_VST=OFF",
     "-DENABLE_WEBSOCKET=OFF",
     "-DENABLE_WEBRTC=OFF",
     "-DENABLE_RNNOISE=OFF",
-    "-DENABLE_SPEEXDSP=OFF"
+    "-DENABLE_SPEEXDSP=OFF",
+    "-DENABLE_FREETYPE=OFF",
+
+    # Windows
+    # Plugins
+    "-DENABLE_COREAUDIO_ENCODER=OFF",
+
+    # Features
+    "-DENABLE_VIRTUALCAM=OFF",
+    "-DENABLE_NVAFX=OFF",
+    "-DENABLE_NVVFX=OFF"
 )
 
-cmake -S . -B ./build-windows/ @Flags
-cmake --build ./build-windows/
-
+cmake -S ./obs-studio/ -B ./obs-studio/build-windows/ @Flags
+cmake --build ./obs-studio/build-windows/
--- external/build_obs.sh
+++ external/build_obs.sh
@@ -1,5 +1,33 @@
-cd obs-studio/
-rm -rf ./build-linux/
-flags="-DENABLE_FRONTEND=OFF -DENABLE_UI=OFF -DENABLE_SCRIPTING=OFF -DENABLE_SPEEXDSP=OFF -DENABLE_VIRTUALCAM=OFF -DENABLE_RNNOISE=OFF -DENABLE_AJA=OFF -DENABLE_VLC=OFF -DENABLE_BROWSER=OFF -DENABLE_WEBSOCKET=OFF -DENABLE_WEBRTC=OFF -DENABLE_DECKLINK=OFF -DENABLE_FREETYPE=OFF -DENABLE_VST=OFF"
-cmake -S . -B ./build-linux/ -DBUILD_SHARED_LIBS=OFF $flags
-cmake --build ./build-linux/
+rm -rf ./obs-studio/build-linux/
+
+flags=(
+    # Core
+    "-DENABLE_UI=OFF"
+    "-DENABLE_SCRIPTING=OFF"
+    "-DENABLE_FRONTEND=OFF"
+
+    # Plugins
+    "-DENABLE_AJA=OFF"
+    "-DENABLE_DECKLINK=OFF"
+    "-DENABLE_SPEEXDSP=OFF"
+    "-DENABLE_BROWSER=OFF"
+    "-DENABLE_VLC=OFF"
+    "-DENABLE_VST=OFF"
+    "-DENABLE_WEBSOCKET=OFF"
+    "-DENABLE_WEBRTC=OFF"
+    "-DENABLE_RNNOISE=OFF"
+    "-DENABLE_FREETYPE=OFF"
+
+    # Linux
+    # Plugins
+    "-DENABLE_PIPEWIRE=OFF"
+    "-DENABLE_V4L2=OFF"
+    "-DENABLE_SNDIO=OFF"
+    "-DENABLE_ALSA=OFF"
+
+    # Features
+    "-DENABLE_WAYLAND=OFF"
+)
+
+cmake -S ./obs-studio/ -B ./obs-studio/build-linux/ "${flags[@]}"
+cmake --build ./obs-studio/build-linux/
