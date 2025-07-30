e8930d4fbd6031df27fcfcf41ac0d56759e5dfe7
Faleh Alotaibi <121350271+f-alotaibi@users.noreply.github.com>
2025-07-30 23:06:33 +0300

    (windows) forgot to use windeployqt when packaging to the build

--- build-scripts/package_windows.ps1
+++ build-scripts/package_windows.ps1
@@ -3,3 +3,4 @@ mkdir build/windows_package/
 cp -r external/obs-studio/build-windows/rundir/Debug build/windows_package/replayer
 cp external/obs-studio/.deps/obs-deps-2025-07-11-x64/bin/*.dll build/windows_package/replayer/bin/64bit/
 cp build/Debug/replayer.exe build/windows_package/replayer/bin/64bit/
+windeployqt.exe build\windows_package\replayer\bin\64bit\replayer.exe
